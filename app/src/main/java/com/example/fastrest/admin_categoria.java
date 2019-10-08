61748baa9a369769988fc35c39a846223f707a37
nicollisoza <nicolld.isozac@utadeo.edu.co>
2019-10-08 10:04:22 -0500

    Tomar fotos o seleccionar de galeria

--- app/src/main/java/com/example/fastrest/admin_categoria.java
+++ app/src/main/java/com/example/fastrest/admin_categoria.java
@@ -1,17 +1,67 @@
 package com.example.fastrest;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 
 public class admin_categoria extends AppCompatActivity {
+
+    private ArrayList<String> categorias = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapterCategoria;
+    private ListView listaCategoria;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_categoria);
+        listaCategoria = findViewById(R.id.categ);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        arrayAdapterCategoria = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categorias);
+        listaCategoria.setAdapter(arrayAdapterCategoria);
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String datos = dataSnapshot.getValue(categoria.class).toString();
+                categorias.add(datos);
+                arrayAdapterCategoria.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
     public void ingresoAdminModCategoria(View view){
         Intent intent = new Intent(admin_categoria.this, admin_mod_categoria.class );
--- app/src/main/java/com/example/fastrest/admin_mod_producto.java
+++ app/src/main/java/com/example/fastrest/admin_mod_producto.java
@@ -1,18 +1,103 @@
 package com.example.fastrest;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class admin_mod_producto extends AppCompatActivity {
+
+    private String APP_DIRECTORY = "myPicture/";
+    private String MEDIA_DIRECTORY = APP_DIRECTORY+ "media";
+    private String TEMPORAL_PICTURE_NAME = "temporal";
+
+    private final int PHOTO_CODE = 100;
+    private final int SELECT_PICTURE = 200;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_mod_producto);
+        imageView = findViewById(R.id.foto);
+        Button button = findViewById(R.id.cargar);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final CharSequence[] opcion = {"Tomar foto","Galeria", "Salir"};
+                final AlertDialog.Builder builder = new AlertDialog.Builder(admin_mod_producto.this);
+                builder.setTitle("Elige una opción");
+                builder.setItems(opcion, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(opcion[i] == "Tomar foto"){
+                            dispatchTakePictureIntent();
+                        }
+                        else if(opcion[i] == "Galeria"){
+                            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            intent.setType("image/*");
+                            startActivityForResult(intent.createChooser(intent, "Selecciona app"), SELECT_PICTURE);
+                        }
+                        else if (opcion[i] == "Salir"){
+                            dialogInterface.dismiss();
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case REQUEST_IMAGE_CAPTURE:
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imageView.setImageBitmap(imageBitmap);
+                }
+                break;
+            case SELECT_PICTURE:
+                if(resultCode == RESULT_OK){
+                    Uri path = data.getData();
+                    imageView.setImageURI(path);
+                }
+                break;
+        }
+    }
+
+    private void decodeBitMap(String dir) {
+        Bitmap bitmap;
+        bitmap = BitmapFactory.decodeFile(dir);
+        imageView.setImageBitmap(bitmap);
+    }
+
     public void ingresoAdminProducto(View view){
         Intent intent = new Intent(admin_mod_producto.this, admin_producto.class );
         startActivity(intent);
